71f7fd2d8ab98fc6e1bb7640ebc374e2d1244801
Faisalz2 <116765507+Faisalz2@users.noreply.github.com>
2024-03-28 01:16:00 +0300

    Update models.py
    New function to calculate calories in model file

--- BodyProject/WorkOut/models.py
+++ BodyProject/WorkOut/models.py
@@ -5,8 +5,15 @@ class Body_info(models.Model):
     Height = models.IntegerField("enter your height")
     Weight = models.IntegerField("enter your weight")
     age = models.IntegerField("enter your age")
-    calories = ''
 
+    def calculate_calories(self):
+        
+        bmr = 10 * self.Weight + 6.25 * self.Height - 5 * self.age + 5
+        calories = bmr * 1.2  
+        return calories
+
+    def __str__(self):
+        return self.name
 
 
 
