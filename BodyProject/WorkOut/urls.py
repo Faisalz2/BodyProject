ee10958e115d75511b1a4ef84f3d85eabd903022
Faisalz2 <116765507+Faisalz2@users.noreply.github.com>
2024-03-28 01:19:25 +0300

    Update urls.py
    Just insert the new function url and delete BodyList

--- BodyProject/WorkOut/urls.py
+++ BodyProject/WorkOut/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
 path('', views.wlcpag, name = 'wlc'),
-path('d',views.BodyList,name='User'),
+path('d',views.calculate_calories_view,name='User'),
 path('login/', views.user_login, name='login'),
 path('signup/', views.user_signup, name='signup'),
 path('logout/', views.user_logout, name='logout'),
