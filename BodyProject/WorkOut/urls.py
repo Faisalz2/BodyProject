55f183568579b2a340ebf5fe8a5d68b8f1b9f70c
Faisalz2 <116765507+Faisalz2@users.noreply.github.com>
2024-04-25 21:35:34 +0300

    Update urls.py

--- BodyProject/WorkOut/urls.py
+++ BodyProject/WorkOut/urls.py
@@ -7,5 +7,5 @@ path('d',views.calculate_calories_view,name='User'),
 path('login/', views.user_login, name='login'),
 path('signup/', views.user_signup, name='signup'),
 path('logout/', views.user_logout, name='logout'),
-
+path('pl/', views.Create, name='pl'),
 ]
